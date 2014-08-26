f87852425f14387d2bb5b7f48372648d3e1a1a2f
Jon Engelbert <jon.a.engelbert@gmail.com>
2014-08-26 12:04:09 -0400

    adding tests and infrastructure for new events

--- spec/views/alpha_events/index.html.erb_spec.rb
+++ spec/views/alpha_events/index.html.erb_spec.rb
@@ -2,7 +2,18 @@ require 'spec_helper'
 
 describe 'alpha_events/index', type: :view do
   it "renders main attributes" do
+    event1= double( start_planned: '25/08/14 10:00',
+              title: 'event 1',
+              tags: 'websiteone, pp, ruby',
+              agenda: 'Refactor index spec',
+              comments: 'Bring your own laptop' )
+    event2= double( start_planned: '10/09/14 16:45',
+              title: 'event 2',
+              tags: 'autograder, client, deploy',
+              agenda: 'finish the rag feature',
+              comments: 'Show the client the latest UI' )
+    @alphaEvents = [event1, event2]
     render
-    expect(rendered).to have_text('event name')
+    expect(rendered).to have_text('Title')
   end
 end
