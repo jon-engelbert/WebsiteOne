e1697bc9ce752db289449d253901732178a82f0b
Jon Engelbert <jon.a.engelbert@gmail.com>
2014-08-27 10:35:52 -0400

    clean up changes to basic_steps that didn't work out, and remove url_for_me from alpha_event model

--- app/models/alpha_event.rb
+++ app/models/alpha_event.rb
@@ -1,9 +1,2 @@
 class AlphaEvent < ActiveRecord::Base
-  def url_for_me(action)
-    if action == 'show'
-      "/articles/#{self.to_param}"
-    else
-      "/articles/#{self.to_param}/#{action}"
-    end
-  end
 end
