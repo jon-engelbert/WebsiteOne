b8038e0f8d5eb75d982dde249e44aab90556aff6
Jon Engelbert <jon.a.engelbert@gmail.com>
2014-09-01 11:16:11 -0400

    starting on hangouts controller and view extensions, building out new infrastructure

--- app/controllers/hookups_controller.rb
+++ app/controllers/hookups_controller.rb
@@ -1,6 +1,13 @@
 class HookupsController < ApplicationController
+  def new
+    @hangout = Hangout.new(planned_start: Time.now.utc, planned_duration: 30)
+  end
+
   def index
     @pending_hookups = Event.pending_hookups
     @active_pp_hangouts = Hangout.pp_hangouts.started.live
+  end
+
+  def create
   end
 end
