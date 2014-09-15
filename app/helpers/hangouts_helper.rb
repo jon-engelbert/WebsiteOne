eb0be8bf3563a62cb1de9c7b115315f9f4af04f0
Jon Engelbert <jon.a.engelbert@gmail.com>
2014-09-15 16:36:41 -0400

    removed obsolete helper method

--- app/helpers/hangouts_helper.rb
+++ app/helpers/hangouts_helper.rb
@@ -3,17 +3,4 @@ module HangoutsHelper
     project_id ||= '00'
     "#{user.id}#{project_id}#{Time.now.to_i}"
   end
-
-  def edit_unsaved_hangout_path(hangout)
-    if hangout.id.present?
-      edit_hangout_path(hangout.id)
-    else
-      tempPath= edit_hangouts_path(title: hangout.title,
-                                   start_planned: hangout.start_planned,
-                                   category: hangout.category,
-                                   description: hangout.description,
-                                   duration_planned: hangout.duration_planned,
-                                   event_id: hangout.event_id)
-    end
-  end
 end
